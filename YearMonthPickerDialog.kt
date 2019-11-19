f872e20063492a09467a1b79cba422e14914d0e4
yar0316_note <yaworkon@gmail.com>
2019-11-19 17:31:40 +0900

    initial commit

---
+++ YearMonthPickerDialog.kt
@@ -0,0 +1,109 @@
+package com.yar0316.alarmapp.util
+
+/**
+ * Kotlinのカスタムピッカーダイアログ
+ * 表示するのは年月のみ選択のPicker
+ * {@link MonthYearPickerView}とセットで使用
+ * @author yar0316
+ */
+import android.content.Context
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+import java.util.*
+import java.util.Calendar.MONTH
+import java.util.Calendar.YEAR
+
+const val TEXT_DEFAULT_POSITIVE = "OK"
+const val TEXT_DEFAULT_NEGATIVE = "Cancel"
+
+class MonthYearPickerDialog(
+    mContext: Context,
+    pickerTitle: String,
+    initialYear: Int,
+    initialMonth: Int
+):AlertDialog(mContext) {
+    private var mPickerView: MonthYearPickerView = MonthYearPickerView(mContext)
+    private var mBuilder: Builder
+
+    init {
+        mPickerView.setYearMonth(initialYear, initialMonth)
+        mBuilder = Builder(mContext).setView(mPickerView)
+            .setTitle(pickerTitle)
+    }
+
+    /**
+     * 年月の初期値が渡されなければとりあえず今月
+     */
+    constructor(mContext: Context, title: String) : this(
+        mContext,
+        title,
+        Calendar.getInstance().get(YEAR),
+        Calendar.getInstance().get(MONTH)
+    )
+
+    override fun show() {
+        mBuilder.show()
+    }
+
+    /**
+     * OKボタンのリスナー追加
+     * 忘れるとOKボタンが出てこないので注意
+     * @param listener ボタンクリック時のコールバック
+     */
+    fun setPositiveButton(listener: DialogInterface.OnClickListener) {
+        mBuilder.setPositiveButton(TEXT_DEFAULT_POSITIVE, listener)
+    }
+
+    /**
+     * ボタンのテキストも決めたい時に使用（valuesに定義している値のリソースIDを渡す）
+     * @param textId ボタンテキストのリソースID
+     * @param listener リスナー
+     */
+    fun setPositiveButton(textId: Int, listener: DialogInterface.OnClickListener){
+        mBuilder.setPositiveButton(textId, listener)
+    }
+
+    /**
+     * ボタンのテキストも決めたい時に使用（まんま文字列を渡す）
+     * @param text ボタンのテキスト
+     * @param listener リスナー
+     */
+    fun setPositiveButton(text: String, listener: DialogInterface.OnClickListener){
+        mBuilder.setPositiveButton(text, listener)
+    }
+
+    /**
+     * キャンセルボタンのリスナー追加
+     * 使わなかった場合ボタンが出てこない
+     * @param listener ボタンクリック時のコールバック
+     */
+    fun setNegativeButton(listener: DialogInterface.OnClickListener){
+        mBuilder.setNegativeButton(TEXT_DEFAULT_NEGATIVE, listener)
+    }
+
+    /**
+     * ボタンのテキストも決めたい時に使用（valuesに定義している値のリソースIDを渡す）
+     * @param textId ボタンテキストのリソースID
+     * @param listener リスナー
+     */
+    fun setNegativeButton(textId: Int , listener: DialogInterface.OnClickListener){
+        mBuilder.setNegativeButton(textId, listener)
+    }
+
+    /**
+     * ボタンのテキストも決めたい時に使用（まんま文字列を渡す）
+     * @param text ボタンのテキスト
+     * @param listener リスナー
+     */
+    fun setNegativeButton(text: String, listener: DialogInterface.OnClickListener){
+        mBuilder.setNegativeButton(text, listener)
+    }
+
+    /**
+     * ダイアログで使用してるViewを返す
+     * 閉じる時に選択されたデータを取得するとなると、私の能力ではこれないと書けなかった
+     */
+    fun getPickerView(): MonthYearPickerView{
+        return mPickerView
+    }
+}
---
+++ YearMonthPickerView.kt
@@ -0,0 +1,54 @@
+package com.yar0316.alarmapp.util
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.NumberPicker
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.yar0316.alarmapp.R
+import java.util.*
+
+private var MIN_YEAR = 2000
+private var MAX_YEAR = 2199
+private val MONTHS = arrayOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+class MonthYearPickerView(mContext: Context): ConstraintLayout(mContext) {
+    private var monthPicker: NumberPicker
+    private var yearPicker: NumberPicker
+
+    init {
+        LayoutInflater.from(mContext).inflate(R.layout.fragment_month_year_dialog, this)
+
+        // pickerの初期値は今月になるように
+        val calendar = Calendar.getInstance()
+        monthPicker = findViewById(R.id.monthPicker)
+        yearPicker = findViewById(R.id.yearPicker)
+        monthPicker.value = calendar.get(Calendar.MONTH)
+        yearPicker.value = calendar.get(Calendar.YEAR)
+
+        // 各Pickerの設定
+        monthPicker.displayedValues = MONTHS
+        monthPicker.minValue = 0
+        monthPicker.maxValue = MONTHS.size - 1
+        yearPicker.minValue = MIN_YEAR
+        yearPicker.maxValue = MAX_YEAR
+    }
+
+    fun setYearMonth(year: Int, month: Int) {
+        yearPicker.value = year
+        monthPicker.value = month
+    }
+
+    /**
+     * 選択された年と月の数値を返す
+     * @return first: 年 second: 月
+     */
+    fun getYearMonth(): Pair<Int, Int>{
+        return Pair(yearPicker.value, monthPicker.value + 1)
+    }
+}
